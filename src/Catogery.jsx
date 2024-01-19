c22d934af1abbcf0cd337fe091153aecd945fbc1
fayisvtp <fayizvtp@gmail.com>
2024-01-19 12:14:02 +0530

    new

--- src/Catogery.jsx
+++ src/Catogery.jsx
@@ -1,83 +1,74 @@
-import React from 'react'
-import { MdHealthAndSafety } from "react-icons/md";
-import { MdOutlineHealthAndSafety } from "react-icons/md";
-import { GiHealthCapsule } from "react-icons/gi";
-import { GiHealthDecrease } from "react-icons/gi";
-import { GiHealthPotion } from "react-icons/gi";
-import { RiHealthBookFill } from "react-icons/ri";
+import React from 'react';
+import { MdHealthAndSafety, MdOutlineHealthAndSafety } from 'react-icons/md';
+import { GiHealthCapsule, GiHealthDecrease, GiHealthPotion } from 'react-icons/gi';
+import { RiHealthBookFill } from 'react-icons/ri';
 
-
-export default function Catogery() {
+export default function Category() {
   return (
-    <div className="catogery  row flex-column-reverse flex-md-row p-3">
-       
-        
-       <div className="input_div col-5 " style={{backgroundColor:'#EBF4FB'}}>
-  <h2 className="doctors_heading justify-content-center text-center">Meet Your Doctors</h2>
-  <div className="input_area  ">
-    <div className='align-item-center d-flex flex-column align-items-center m-4' >
-    <input  style={{ width: '90%',backgroundColor:'#D9D9D9' }} type="text" placeholder='Enter Your name' /><br /><br />
-    <input  style={{ width: '90%',backgroundColor:'#D9D9D9'  }} type="text" placeholder='Enter Your contact' /><br /><br />
-    <div className="d-flex col-12">
-          <select name="doctor" id="selectDoctor" style={{ marginRight: '10px',width:'50%',backgroundColor:'#D9D9D9'  }}>
-          <option value="doctor1">Doctor Name</option>
-            <option value="doctor1">Doctor 1</option>
-            <option value="doctor2">Doctor 2</option>
-            <option value="doctor3">Doctor 3</option>
-          </select>
+    <div className="category row flex-column-reverse flex-md-row p-3">
+      <div className="input_div col-12 col-md-5 mb-3 mb-md-0" style={{ backgroundColor: '#EBF4FB' }}>
+        <h2 className="doctors_heading justify-content-center text-center">Meet Your Doctors</h2>
+        <div className="input_area">
+          <div className="align-item-center d-flex flex-column align-items-center m-4">
+            <input style={{ width: '90%', backgroundColor: '#D9D9D9' }} type="text" placeholder="Enter Your name" /><br /><br />
+            <input style={{ width: '90%', backgroundColor: '#D9D9D9' }} type="text" placeholder="Enter Your contact" /><br /><br />
+            <div className="d-flex col-12">
+              <select name="doctor" id="selectDoctor" style={{ marginRight: '10px', width: '50%', backgroundColor: '#D9D9D9' }}>
+                <option value="doctor1">Doctor Name</option>
+                <option value="doctor1">Doctor 1</option>
+                <option value="doctor2">Doctor 2</option>
+                <option value="doctor3">Doctor 3</option>
+              </select>
 
-          <select name="department" id="selectDepartment" style={{width:'50%',backgroundColor:'#D9D9D9' }}>
-          <option value="department1">Department </option>
-            <option value="department1">Department 1</option>
-            <option value="department2">Department 2</option>
-            <option value="department3">Department 3</option>
-          </select>
-        </div>
-        </div>
-  </div>
-</div>
-
-<div className="icons_area col-7   text-light">
-      <div className="row p-3">
-        <div className="icons col-3 d-flex flex-column align-items-center ">
-          <MdHealthAndSafety />
-          <p className="icon_text">lorum ipsum</p>
-        </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-        <MdOutlineHealthAndSafety />
-          <p className="icon_text">lorum ipsum</p>
-        </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-        <GiHealthCapsule />
-          <p className="icon_text">lorum ipsum</p>
-        </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-        <GiHealthDecrease />
-          <p className="icon_text">lorum ipsum</p>
+              <select name="department" id="selectDepartment" style={{ width: '50%', backgroundColor: '#D9D9D9' }}>
+                <option value="department1">Department </option>
+                <option value="department1">Department 1</option>
+                <option value="department2">Department 2</option>
+                <option value="department3">Department 3</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="icons col-3 d-flex flex-column align-items-center">
-        <GiHealthPotion />
-          <p className="icon_text">lorum ipsum</p>
-        </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-          <MdHealthAndSafety />
-          <p className="icon_text">lorum ipsum</p>
-        </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-        <RiHealthBookFill />
 
-          <p className="icon_text">lorum ipsum</p>
+      <div className="icons_area col-12 col-md-7 text-light">
+        <div className="row p-3">
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <MdHealthAndSafety />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <MdOutlineHealthAndSafety />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <GiHealthCapsule />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <GiHealthDecrease />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
         </div>
-        <div className="icons col-3 d-flex flex-column align-items-center">
-          <MdHealthAndSafety />
-          <p className="icon_text">lorum ipsum</p>
+        <div className="row">
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <GiHealthPotion />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <MdHealthAndSafety />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <RiHealthBookFill />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
+          <div className="icons col-3 d-flex flex-column align-items-center">
+            <MdHealthAndSafety />
+            <p className="icon_text">lorum ipsum</p>
+          </div>
         </div>
       </div>
     </div>
-        
-      
-    </div>
-  )
+  );
 }
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -10,22 +10,23 @@ function Gallery() {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
- 
+    flexWrap: 'wrap', 
   };
 
   const imageStyle = {
-    width: '100vh',
-    height: '190px',
+    width: '100%', 
+    maxWidth: '300px', 
+    height: 'auto', 
     borderRadius: '10%',
     transform: 'rotate(-6.683deg)',
     margin: '10px',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)'
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
   };
 
   const images = [image, image2, image3, image4];
 
   return (
-    <div  style={cardStyle}>
+    <div style={cardStyle}>
       {images.map((image, index) => (
         <img key={index} src={image} alt={`Image ${index + 1}`} style={imageStyle} />
       ))}
